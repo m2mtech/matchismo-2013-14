49fefd5887de30954a8ffb6fdb558f24533cff26
m2m tech <tech@m2m.at>
2013-11-29 21:21:02 +0100

    assignment #4 task #5

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -118,8 +118,17 @@
 - (void)touchCard:(UITapGestureRecognizer *)gesture
 {
     if (gesture.state == UIGestureRecognizerStateEnded) {
-        [self.game chooseCardAtIndex:gesture.view.tag];
-        [self updateUI];
+        Card *card = [self.game cardAtIndex:gesture.view.tag];
+        [UIView transitionWithView:gesture.view
+                          duration:0.5
+                           options:UIViewAnimationOptionTransitionFlipFromLeft animations:^{
+                               card.chosen = !card.chosen;
+                               [self updateView:gesture.view forCard:card];
+                           } completion:^(BOOL finished) {
+                               card.chosen = !card.chosen;
+                               [self.game chooseCardAtIndex:gesture.view.tag];
+                               [self updateUI];
+                           }];
     }
 }
 
